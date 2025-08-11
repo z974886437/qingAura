a100a14c6998188cc4f4f2044932640d10176612
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-08-11 19:48:19 +0800

    Aura Projectile Spell

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,21 @@
+// 青楼
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	//激活能力
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
