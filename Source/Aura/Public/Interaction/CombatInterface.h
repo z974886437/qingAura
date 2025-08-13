ba7e051aedb476f31a33b7aeb312515d964a36e3
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-08-13 19:27:47 +0800

    Motion Warping

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -7,7 +7,7 @@
 #include "CombatInterface.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI,BlueprintType)
 class UCombatInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -24,4 +24,7 @@ class AURA_API ICombatInterface
 public:
 	virtual int32 GetPlayerLevel();
 	virtual FVector GetCombatSocketLocation();//获取战斗插槽位置
+
+	UFUNCTION(BlueprintImplementableEvent,BlueprintCallable)
+	void UpdataFacingTarget(const FVector& Target);//更新面向目标
 };
