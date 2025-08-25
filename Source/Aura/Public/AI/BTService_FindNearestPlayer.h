a0cd41696c5da39dc407c5c29d70c533a78659fc
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-08-25 19:23:07 +0800

    Blackboard Keys

--- Source/Aura/Public/AI/BTService_FindNearestPlayer.h
+++ Source/Aura/Public/AI/BTService_FindNearestPlayer.h
@@ -16,4 +16,10 @@ class AURA_API UBTService_FindNearestPlayer : public UBTService_BlueprintBase
 
 protected:
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp,uint8* NodeMemory,float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly,EditAnywhere)
+	FBlackboardKeySelector TargetToFollowSelector;//目标跟随选择器
+
+	UPROPERTY(BlueprintReadOnly,EditAnywhere)
+	FBlackboardKeySelector DistanceToTargetSelector;//到目标选择器的距离 
 };
