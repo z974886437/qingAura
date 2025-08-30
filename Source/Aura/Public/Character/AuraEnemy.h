0ec50374d833cb2f9f8de20addc918d6713cc613
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-08-30 19:03:52 +0800

    Ghoul Attack Montages

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -46,7 +46,7 @@ public:
 	UPROPERTY(BlueprintReadOnly,Category = "Combat")
 	bool bHitReacting = false;//B命中反应
 
-	UPROPERTY(BlueprintReadOnly,Category = "Combat")
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Combat")
 	float BaseWalkSpeed = 250.f;
 
 	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Combat")
