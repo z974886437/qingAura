26f7a75c39061e1b6a07c08aadbb9e95a19a37c9
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-09-22 16:54:14 +0800

    Showing Abilities in the Spell Tree

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -35,7 +35,8 @@ void UAuraWidgetController::BroadcastAbilityInfo()
 	{
 		// 1. 根据技能实例 (AbilitySpec) 拿到技能标签 (Abilities.xxx)
 		FAuraAbilityInfo Info = AbilityInfo->FindAbilityInfoForTag(AuraAbilitySystemComponent->GetAbilityTagFromSpec(AbilitySpec));
-		Info.InputTag = AuraAbilitySystemComponent->GetInputTagFromSpec(AbilitySpec);// 2. 再拿到该技能对应的输入标签 (InputTag.Q / InputTag.LMB)
+		Info.InputTag = AuraAbilitySystemComponent->GetInputTagFromSpec(AbilitySpec);// 2. 拿到该技能对应的输入键位（例如 Q 键，鼠标左键）
+		Info.StatusTag = AuraAbilitySystemComponent->GetStatusFromSpec(AbilitySpec);// 3. 拿到该技能当前的状态（比如是否解锁、是否冷却中）
 		AbilityInfoDelegate.Broadcast(Info); // 3. 把完整的技能信息广播出去，让 UI 刷新（比如技能栏显示图标/冷却）
 	});
 	GetAuraASC()->ForEachAbility(BroadcastDelegate);// 遍历所有技能，逐个执行上面绑定的 Lambda
