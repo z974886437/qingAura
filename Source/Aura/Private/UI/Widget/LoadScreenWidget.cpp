9cf423ca9ba7f1958f4fc0c0e91b9bfbbd61c76c
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-11-02 19:54:31 +0800

    View Model Class

---
+++ Source/Aura/Private/UI/Widget/LoadScreenWidget.cpp
@@ -0,0 +1,5 @@
+// 青楼
+
+
+#include "UI/Widget/LoadScreenWidget.h"
+
---
+++ Source/Aura/Public/UI/HUD/LoadScreenHUD.h
@@ -0,0 +1,17 @@
+// 青楼
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "LoadScreenHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ALoadScreenHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
