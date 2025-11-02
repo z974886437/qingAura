394c365f98a2d80a157309acf9918514932306b1
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-11-02 20:51:05 +0800

    Constructing a View Model

--- Source/Aura/Public/UI/HUD/LoadScreenHUD.h
+++ Source/Aura/Public/UI/HUD/LoadScreenHUD.h
@@ -6,6 +6,8 @@
 #include "GameFramework/HUD.h"
 #include "LoadScreenHUD.generated.h"
 
+class UMVVM_LoadScreen;
+class ULoadScreenWidget;
 /**
  * 
  */
@@ -13,5 +15,21 @@ UCLASS()
 class AURA_API ALoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;//加载屏幕小部件类
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ULoadScreenWidget> LoadScreenWidget;//加载屏幕小部件
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadScreen> LoadScreenViewModelClass;//加载屏幕视图模型类
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UMVVM_LoadScreen> LoadScreenViewModel;//加载屏幕视图模型
+protected:
+
+	virtual void BeginPlay() override;
 };
