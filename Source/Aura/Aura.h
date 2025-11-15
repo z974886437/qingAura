ee2932b861f99b5e831c953c732ce041005f8b5e
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-11-15 17:06:27 +0800

    Different Highlight Colors

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -6,6 +6,8 @@
 
 // 定义一个名为 CUSTOM_DEPTH_RED 的宏，值为 250
 #define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_BLUE 251
+#define CUSTOM_DEPTH_TAN 250
 
 // 定义一个名为 ECC_Projectile 的宏，代表 GameTraceChannel1（自定义碰撞通道1）
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
